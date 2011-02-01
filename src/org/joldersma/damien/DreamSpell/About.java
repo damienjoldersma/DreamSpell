0aaf20461c60f5ce9eeddab24a41261998739e9d
Damien Joldersma <damien@joldersma.org>
2011-02-01 00:17:38 -0800

    Updated About tab: refactor from TextView into static htlm WebView

--- src/org/joldersma/damien/DreamSpell/About.java
+++ src/org/joldersma/damien/DreamSpell/About.java
@@ -5,6 +5,7 @@ import java.text.DateFormat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,5 +19,9 @@ public class About extends Activity {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG,"** ON CREATE **");
 		setContentView(R.layout.about);
+		
+		WebView mWebView = (WebView) findViewById(R.id.WebView01);
+	    mWebView.getSettings().setJavaScriptEnabled(true);
+	    mWebView.loadUrl("file:///android_asset/about.html");
 	}
 }
