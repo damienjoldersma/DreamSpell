19e36a7ef6b9dbc3a0519e5fd671b17feb21280a
Damien Joldersma <damien@joldersma.org>
2011-02-16 00:06:21 -0800

    wow, math.  completely amazing, never thought I would ever personally apply a modulo operator without direction to do so, amazing journey this app

--- src/org/joldersma/damien/DreamSpell/Tzolkin.java
+++ src/org/joldersma/damien/DreamSpell/Tzolkin.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.ImageView.ScaleType;
 import android.widget.TableRow.LayoutParams;
 import android.webkit.WebView;
 import android.widget.ImageView;
@@ -26,20 +28,26 @@ public class Tzolkin extends Activity {
 		
 		/* Find Tablelayout defined in main.xml */
 		TableLayout tl = (TableLayout)findViewById(R.id.tzolkin_table);
-		
+		int kin = 1;
 		for (int seal = 1; seal <= 20; seal++)
 		{
 			/* Create a new row to be added. */
 			TableRow tr = new TableRow(this);
-			tr.setLayoutParams(new LayoutParams(
-								LayoutParams.FILL_PARENT,
-								LayoutParams.WRAP_CONTENT));
+			
+			
+			
+			tr.setLayoutParams(new TableRow.LayoutParams(
+					TableRow.LayoutParams.FILL_PARENT,
+					TableRow.LayoutParams.WRAP_CONTENT));
 			
 			ImageView imageView = new ImageView(this);
 			imageView.setImageResource(AndroidUtil.getGlyphResource(seal));
-			imageView.setLayoutParams(new LayoutParams(
-								LayoutParams.FILL_PARENT,
-								LayoutParams.WRAP_CONTENT));
+			imageView.setScaleType(ScaleType.FIT_XY);
+		
+			imageView.setLayoutParams(new LayoutParams(40,40));
+//			imageView.setLayoutParams(new LayoutParams(
+//								LayoutParams.FILL_PARENT,
+//								LayoutParams.WRAP_CONTENT));
 			
 			imageView.setPadding(3, 1, 3, 1);
 			/* Add TextView to row. */
@@ -47,15 +55,18 @@ public class Tzolkin extends Activity {
 			
 			for (int tone = 1; tone <= 13; tone++) {
 				
-				Log.d(TAG, "Going to add text view");
+				Log.d(TAG, "Going to add kin text view: ");
+				
+				kin = (seal + ((tone-1) * 20)) % 13;
+				if ( kin == 0 ) kin = 13; // hack hack but otherwise 1-12 work, but 13 is 0 - I have poor math skills
+				
 				/* Create a TextView to be the row-content. */
 				TextView textView = new TextView(this);
-				textView.setText(String.format("%s",tone));
-				textView.setLayoutParams(new LayoutParams(
-									LayoutParams.FILL_PARENT,
-									LayoutParams.WRAP_CONTENT));
+				textView.setText(String.format("%s",kin));
+				textView.setGravity(Gravity.CENTER);
+				textView.setLayoutParams(new LayoutParams(21,40));
 				textView.setTextColor(Color.BLACK);
-				textView.setPadding(3, 1, 3, 1);
+				//textView.setPadding(1, 1, 1, 1);
 				/* Add TextView to row. */
 				tr.addView(textView);
 			
